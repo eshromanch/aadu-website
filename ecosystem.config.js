d8420a0739d74bf6716c88970c7f48cdc266da3a
eshromanch <romanch.haq868@gmail.com>
2025-08-25 14:50:25 +0200

    trigger: deployment to fix environment variables

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: 'aadu-website',
+    script: 'npm',
+    args: 'start',
+    cwd: './app',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true
+  }]
+};
